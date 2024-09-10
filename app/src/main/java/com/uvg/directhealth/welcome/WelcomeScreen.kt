3c13d00c9a090ccdc698e65b38fc37bb53ce8d95
juansolm <juandsolism@gmail.com>
2024-09-09 20:45:57 -0600

    Agregando el diseño de la pantalla Welcome

---
+++ app/src/main/java/com/uvg/directhealth/welcome/WelcomeScreen.kt
@@ -0,0 +1,125 @@
+package com.uvg.directhealth.welcome
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.uvg.directhealth.R
+import com.uvg.directhealth.ui.theme.DirectHealthTheme
+
+@Composable
+fun WelcomeScreen(){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp),
+        Arrangement.SpaceBetween,
+        Alignment.CenterHorizontally
+    ){
+        Spacer(modifier = Modifier.weight(0.8f))
+
+        Image(
+            painter = painterResource(id = R.drawable.welcome),
+            contentDescription = "Welcome Image",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(250.dp)
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Column (
+            modifier = Modifier
+                .padding(15.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ){
+            Text(
+                text = stringResource(id = R.string.welcome_title),
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 35.sp,
+                    lineHeight = 40.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+            Text(
+                text = stringResource(id = R.string.welcome_text),
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    fontSize = 14.sp,
+                    lineHeight = 24.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Column (
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            TextButton(
+                onClick = {  },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(MaterialTheme.colorScheme.primary)
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.login_button),
+                    style = MaterialTheme.typography.labelLarge.copy(
+                        color = MaterialTheme.colorScheme.onPrimary,
+                    )
+                )
+            }
+
+            TextButton(
+                onClick = {  },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(MaterialTheme.colorScheme.secondaryContainer)
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.register_button),
+                    style = MaterialTheme.typography.labelLarge.copy(
+                        color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewInformation() {
+    DirectHealthTheme {
+        Surface {
+            WelcomeScreen()
+        }
+    }
+}
